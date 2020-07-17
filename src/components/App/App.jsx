c2b9eeaa93e2e9551d98461b4806a6c8106981ab
Chelsea Spangler <chelseanspangler@gmail.com>
2020-07-17 10:43:25 -0700

    set up App router

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from '../Header';
+import Home from '../Home';
+import Pubs from '../Pubs';
+import Services from '../Services';
+import Contact from '../Contact';
+import Footer from '../Footer';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Header></Header>
+      <Router>
+        <Route path='/' component={Home} />
+        <Route path='/publications' component={Pubs} />
+        <Route path='/services' component={Services} />
+        <Route path='contact' component={Contact} />
+      </Router>
+      <Footer></Footer>
+    </>
+  );
 }
-  
