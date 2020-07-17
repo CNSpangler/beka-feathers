c77a5dba1a288fac183db0fd16ef2c0f31adad86
Chelsea Spangler <chelseanspangler@gmail.com>
2020-07-17 11:32:45 -0700

    App renders Header, Home, Footer w contact links n Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => (
+  <>
+    <h3>Footer</h3>
+    <a href="https://twitter.com/feathersb?lang=en">Twitter</a>
+    <a href="https://www.linkedin.com/in/beka-feathers-53b3b71a/">LinkedIn</a>
+    <a href="mailto:beka.feathers@gmail.com">Email</a>
+  </>
+);
+
+export default Footer;
---
+++ src/components/NavBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NavBar = () => (
+  <>
+    <h4>NavBar under construction</h4>
+  </>
+);
+
+export default NavBar;
