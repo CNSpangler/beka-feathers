59e74000e10427f76565e4de6f8ccfbbaed87458
Chelsea Spangler <chelseanspangler@gmail.com>
2020-07-24 15:40:16 -0700

    contact icons added

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,20 +4,24 @@ import styles from './Contact.css';
 const Contact = () => (
   <div className={styles.Contact}>
     <div className={styles.contactCard}>
+      <img src="email.png" />
       <a href="mailto:beka.feathers@gmail.com" target="_blank" rel="noreferrer">Email</a>
     </div>
 
     <div className={styles.contactCard}>
-      <a href="mailto:beka.feathers@protonmail.com" target="_blank" rel="noreferrer" className={styles.contactCard}>Secure Email</a>
+      <img src="caps-lock.png" />
+      <a href="mailto:beka.feathers@protonmail.com" target="_blank" rel="noreferrer" >Secure Email</a>
     </div>
 
     <div className={styles.contactCard}>
+      <img src="twitter.png" />
       <a href="https://twitter.com/messages/compose?recipient_id=106555952"
         data-screen-name="@feathersb">Twitter</a>
     </div>
 
     <div className={styles.contactCard}>
-      <a href="https://join.skype.com/invite/DzcouHgYvc4o" className={styles.contactCard}>Skype</a>
+      <img src="skype.png" />
+      <a href="https://join.skype.com/invite/DzcouHgYvc4o">Skype</a>
     </div>
   </div>
 );
