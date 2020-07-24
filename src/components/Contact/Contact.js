7a15fddfbf28b8664b5703e847e38ae7426a064a
Chelsea Spangler <chelseanspangler@gmail.com>
2020-07-24 16:00:18 -0700

    contact buttons centered and styled on Contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,25 +3,27 @@ import styles from './Contact.css';
 
 const Contact = () => (
   <div className={styles.Contact}>
-    <div className={styles.contactCard}>
-      <img src="email.png" />
-      <a href="mailto:beka.feathers@gmail.com" target="_blank" rel="noreferrer">Email</a>
-    </div>
+    <div className={styles.cardContainer}>
+      <div className={styles.contactCard}>
+        <img src="email.png" />
+        <a href="mailto:beka.feathers@gmail.com" target="_blank" rel="noreferrer">Email</a>
+      </div>
 
-    <div className={styles.contactCard}>
-      <img src="caps-lock.png" />
-      <a href="mailto:beka.feathers@protonmail.com" target="_blank" rel="noreferrer" >Secure Email</a>
-    </div>
+      <div className={styles.contactCard}>
+        <img src="caps-lock.png" />
+        <a href="mailto:beka.feathers@protonmail.com" target="_blank" rel="noreferrer" >Secure Email</a>
+      </div>
 
-    <div className={styles.contactCard}>
-      <img src="twitter.png" />
-      <a href="https://twitter.com/messages/compose?recipient_id=106555952"
-        data-screen-name="@feathersb">Twitter</a>
-    </div>
+      <div className={styles.contactCard}>
+        <img src="twitter.png" />
+        <a href="https://twitter.com/messages/compose?recipient_id=106555952"
+          data-screen-name="@feathersb">Twitter</a>
+      </div>
 
-    <div className={styles.contactCard}>
-      <img src="skype.png" />
-      <a href="https://join.skype.com/invite/DzcouHgYvc4o">Skype</a>
+      <div className={styles.contactCard}>
+        <img src="skype.png" />
+        <a href="https://join.skype.com/invite/DzcouHgYvc4o">Skype</a>
+      </div>
     </div>
   </div>
 );
