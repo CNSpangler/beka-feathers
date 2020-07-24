4b47de0eb40a2160b7810445baae0b38822584a2
Chelsea Spangler <chelseanspangler@gmail.com>
2020-07-24 15:31:06 -0700

    apply stylesheet to Contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import styles from './Contact.js';
+import styles from './Contact.css';
 
 const Contact = () => (
-  <div className={StyleSheet.Contact}>
-    <div className={styles.contactContainer}>
+  <div className={styles.Contact}>
+    <div className={styles.contactCard}>
+      <a href="mailto:beka.feathers@gmail.com" target="_blank" rel="noreferrer">Email</a>
+    </div>
 
-      <a href="mailto:beka.feathers@gmail.com" target="_blank" rel="noreferrer" className={styles.contactCard}>Email</a>
-
+    <div className={styles.contactCard}>
       <a href="mailto:beka.feathers@protonmail.com" target="_blank" rel="noreferrer" className={styles.contactCard}>Secure Email</a>
+    </div>
 
+    <div className={styles.contactCard}>
       <a href="https://twitter.com/messages/compose?recipient_id=106555952"
         data-screen-name="@feathersb">Twitter</a>
+    </div>
 
+    <div className={styles.contactCard}>
       <a href="https://join.skype.com/invite/DzcouHgYvc4o" className={styles.contactCard}>Skype</a>
     </div>
   </div>
