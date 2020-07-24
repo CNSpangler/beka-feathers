acf0834db63a59b10475225f65e6f023b3e58814
Chelsea Spangler <chelseanspangler@gmail.com>
2020-07-24 11:20:30 -0700

    Footer icons added, hover buttons fully styled

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,11 +3,24 @@ import styles from './Footer.css';
 
 const Footer = () => (
   <div className={styles.Footer}>
-    <a href="https://twitter.com/feathersb?lang=en" target="_blank" rel="noreferrer">Twitter</a>
-    <span>|</span>
-    <a href="https://www.linkedin.com/in/beka-feathers-53b3b71a/" target="_blank" rel="noreferrer">LinkedIn</a>
-    <span>|</span>
-    <a href="mailto:beka.feathers@gmail.com" target="_blank" rel="noreferrer">Email</a>
+    <div className={styles.contact}>
+      <img src="twitter.png" />
+      <a href="https://twitter.com/feathersb?lang=en" target="_blank" rel="noreferrer">Twitter</a>
+    </div>
+
+    {/* <span>|</span> */}
+
+    <div className={styles.contact}>
+      <img src="linkedin.png" />
+      <a href="https://www.linkedin.com/in/beka-feathers-53b3b71a/" target="_blank" rel="noreferrer">LinkedIn</a>
+    </div>
+
+    {/* <span>|</span> */}
+
+    <div className={styles.contact}>
+      <img src="email.png" />
+      <a href="mailto:beka.feathers@gmail.com" target="_blank" rel="noreferrer">Email</a>
+    </div>
   </div>
 );
 
