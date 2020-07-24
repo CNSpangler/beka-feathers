5ca7269414d82a1aa65236424641607d86076e6d
Chelsea Spangler <chelseanspangler@gmail.com>
2020-07-24 15:23:11 -0700

    add custom links to Contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,10 +4,15 @@ import styles from './Contact.js';
 const Contact = () => (
   <div className={StyleSheet.Contact}>
     <div className={styles.contactContainer}>
-      <a href="" className={styles.contactCard}/>
-      <a href="" className={styles.contactCard}/>
-      <a href="" className={styles.contactCard}/>
-      <a href="" className={styles.contactCard}/>
+
+      <a href="mailto:beka.feathers@gmail.com" target="_blank" rel="noreferrer" className={styles.contactCard}>Email</a>
+
+      <a href="mailto:beka.feathers@protonmail.com" target="_blank" rel="noreferrer" className={styles.contactCard}>Secure Email</a>
+
+      <a href="https://twitter.com/messages/compose?recipient_id=106555952"
+        data-screen-name="@feathersb">Twitter</a>
+
+      <a href="https://join.skype.com/invite/DzcouHgYvc4o" className={styles.contactCard}>Skype</a>
     </div>
   </div>
 );
