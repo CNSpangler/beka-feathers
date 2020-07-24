8aa1d04cc7ddddb24d3ef4cbff3e22e92def5010
Chelsea Spangler <chelseanspangler@gmail.com>
2020-07-23 17:52:49 -0700

    Footer elements center and space

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import styles from './Footer.css';
 
 const Footer = () => (
-  <>
-    <a href="https://twitter.com/feathersb?lang=en">Twitter</a>
-    <a href="https://www.linkedin.com/in/beka-feathers-53b3b71a/">LinkedIn</a>
-    <a href="mailto:beka.feathers@gmail.com">Email</a>
-  </>
+  <div className={styles.Footer}>
+    <a href="https://twitter.com/feathersb?lang=en" target="_blank" rel="noreferrer">Twitter</a>
+    <span>|</span>
+    <a href="https://www.linkedin.com/in/beka-feathers-53b3b71a/" target="_blank" rel="noreferrer">LinkedIn</a>
+    <span>|</span>
+    <a href="mailto:beka.feathers@gmail.com" target="_blank" rel="noreferrer">Email</a>
+  </div>
 );
 
 export default Footer;
