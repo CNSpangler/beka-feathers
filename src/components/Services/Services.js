551927f58ca095a984b39db00717abbda57984e1
Chelsea Spangler <chelseanspangler@gmail.com>
2020-07-27 16:29:37 -0700

    add work philosophy and political development to Services

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -9,9 +9,17 @@ const flipElements = servicesData.map(service => (
 
 const Services = () => (
   <div className={styles.Services}>
-    <h2>services coming soon</h2>
+    <div className={styles.philContainer}>
+      <h5>Work Philosophy:</h5>
+      <p>I am an expert. You can call the UN and ask them, they will confirm. Or I can send you a photo of my official expert badge. Please do not question me.</p>
+    </div>
+    <h4>SERVICES:</h4>
     <div className={styles.flipCards}>
       {flipElements}
+    </div>
+    <div className={styles.polDev}>
+      <h4>What is Political Development?</h4>
+      <p>I am an expert. You can call the UN and ask them, they will confirm. Or I can send you a photo of my official expert badge. Please do not question me.</p>
     </div>
   </div>
 );
