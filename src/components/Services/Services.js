bbc7b8bac6aa82de62db66837e7bf22fe957579e
Chelsea Spangler <chelseanspangler@gmail.com>
2020-07-27 12:28:59 -0700

    add stylesheet to Services

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import FlipCard from '../FlipCard/FlipCard';
 import servicesData from './servicesData.json';
+import styles from './Services.css';
 
 const flipElements = servicesData.map(service => (
   <FlipCard key={service.title}>{service.title}</FlipCard>
 ));
 
 const Services = () => (
-  <>
+  <div className={styles.Services}>
     <h2>services coming soon</h2>
-    <div>
+    <div className={styles.flipCards}>
       {flipElements}
     </div>
-  </>
+  </div>
 );
 
 export default Services;
