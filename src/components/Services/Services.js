5e0856aabb67ba41e4df7c517d6cc75d0092d6ca
Chelsea Spangler <chelseanspangler@gmail.com>
2020-07-27 17:37:53 -0700

    removed grid; classes won't apply to FlipCard

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -3,12 +3,9 @@ import FlipCard from '../FlipCard/FlipCard';
 import servicesData from './servicesData.json';
 import styles from './Services.css';
 
-const flipElements = servicesData.map(service => {
-  const gridName = `styles.${service.class}`;
-  console.log(gridName);
-
-  return <FlipCard key={service.title} title={service.title} description={service.description} className={gridName}>{service.title}</FlipCard>;
-});
+const flipElements = servicesData.map(service => (
+  <FlipCard key={service.title} title={service.title} description={service.description} />
+));
 
 const Services = () => (
   <div className={styles.Services}>
@@ -17,9 +14,7 @@ const Services = () => (
       <p>I am an expert. You can call the UN and ask them, they will confirm. Or I can send you a photo of my official expert badge. Please do not question me.</p>
     </div>
     <h4>SERVICES:</h4>
-    <div className={styles.flipCards}>
-      {flipElements}
-    </div>
+    {flipElements}
     <div className={styles.polDev}>
       <h4>What is Political Development?</h4>
       <p>I am an expert. You can call the UN and ask them, they will confirm. Or I can send you a photo of my official expert badge. Please do not question me.</p>
