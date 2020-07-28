e3f8a920d5e7779212a6f7dddd787dcfb73d62ea
Chelsea Spangler <chelseanspangler@gmail.com>
2020-07-28 10:49:53 -0700

    scaffold JSX for Pubs component

--- src/components/Pubs/Pubs.js
+++ src/components/Pubs/Pubs.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import styles from './Pubs.css';
 
 const Pubs = () => (
-  <>
-    <h2>publications coming soon</h2>
-  </>
+  <div className={styles.Pubs}>
+    <div>
+      <img src="CONSTITUTIONS.png" />
+      <div>
+        <div>
+          <h5>Re:Constitutions<br />Connecting Citizens with the Rules of The Game</h5>
+          <h6>Beka Feathers with art by Kasia Babis</h6>
+          <button>BOOKSHOP</button>
+          <button>AMAZON</button>
+        </div>
+        <div>Coming June 15, 2021</div>
+      </div>
+
+      <p></p>
+      <p></p>
+      <p></p>
+      <p className={styles.review}></p>
+    </div>
+  </div>
 );
 
 export default Pubs;
