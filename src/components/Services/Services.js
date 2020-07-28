dd38134fb427e6e87f3a21df80b179c86d3a9553
Chelsea Spangler <chelseanspangler@gmail.com>
2020-07-27 17:41:21 -0700

    arrange FlipCards with flexbox

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -14,7 +14,9 @@ const Services = () => (
       <p>I am an expert. You can call the UN and ask them, they will confirm. Or I can send you a photo of my official expert badge. Please do not question me.</p>
     </div>
     <h4>SERVICES:</h4>
-    {flipElements}
+    <div className={styles.servicesContainer}>
+      {flipElements}
+    </div>
     <div className={styles.polDev}>
       <h4>What is Political Development?</h4>
       <p>I am an expert. You can call the UN and ask them, they will confirm. Or I can send you a photo of my official expert badge. Please do not question me.</p>
