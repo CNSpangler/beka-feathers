743fe80b329292684aaf634fb1a958f3d4180109
Chelsea Spangler <chelseanspangler@gmail.com>
2020-08-01 10:45:14 -0700

    bioContainer and contents styled for mobile

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,7 +6,9 @@ const Home = () => (
   <div className={styles.Home}>
     <div className={styles.bioContainer}>
       <div className={styles.bio}>
-        <img className={styles.headshot} src="headshot.jpg" alt="Beka Feathers smiling at the camera in an outdoor setting." />
+        <div className={styles.headshotContainer}>
+          <img className={styles.headshot} src="headshot.jpg" alt="Beka Feathers smiling at the camera in an outdoor setting." />
+        </div>
 
         <p>Beka Feathers is a legal adviser on post-conflict institution-building, specializing in constitution-drafting, transitional governments, and peace processes. She was most recently a contractor with the Bureau for Conflict & Stabilization Operations at the U.S. Department of State, where she provided conflict analysis and policy guidance on Yemen. Previously, she was Counsel with the Public International Law & Policy Group (PILPG), where she worked on projects in Yemen, South Sudan, Syria, Burma, Georgia, and Kosovo. She has advised clients on a range of issues, including constitution-drafting, post-conflict transitional governance, peace negotiations, judicial reform, and anti-corruption policy. Beka has also served as a legal adviser to the Montenegrin delegation to the United Nations Human Rights Council. In addition to her work with PILPG, she has done research on foreign agricultural investment for the Centre for Human Rights in South Africa; worked on parliamentary strengthening and democratic benchmarks for the National Democratic Institute; and developed education and transportation policy for the Oregon State Legislature.</p>
 
