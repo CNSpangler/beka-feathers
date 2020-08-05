d54f3307d4d13072fe46ceb78d36c8567461a4c6
Chelsea Spangler <chelseanspangler@gmail.com>
2020-08-05 15:54:52 -0700

    make contact buttons span whole div

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,39 +5,47 @@ const Contact = () => (
   <div className={styles.Contact}>
     <div className={styles.cardContainer}>
       <div className={styles.contactCard}>
-        <img src="email.png" />
         <a 
           href="mailto:beka.feathers@gmail.com" 
           target="_blank" 
           rel="noreferrer"
-        >Email</a>
+        >
+          <img src="email.png" />
+          Email
+        </a>
       </div>
 
       <div className={styles.contactCard}>
-        <img src="caps-lock.png" />
         <a 
           href="mailto:beka.feathers@protonmail.com" target="_blank" 
           rel="noreferrer"
-        >Protonmail</a>
+        >
+          <img src="caps-lock.png" />
+          Protonmail
+        </a>
       </div>
 
       <div className={styles.contactCard}>
-        <img src="twitter.png" />
         <a 
           href="https://twitter.com/messages/compose?recipient_id=106555952"
           data-screen-name="@feathersb"
           target="_blank"
           rel="noreferrer"
-        >Twitter</a>
+        >
+          <img src="twitter.png" />
+          Twitter
+        </a>
       </div>
 
       <div className={styles.contactCard}>
-        <img src="skype.png" />
         <a 
           href="https://join.skype.com/invite/DzcouHgYvc4o"
           target="_blank" 
           rel="noreferrer"
-        >Skype</a>
+        >
+          <img src="skype.png" />
+          Skype
+        </a>
       </div>
     </div>
   </div>
