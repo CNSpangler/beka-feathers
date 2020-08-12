ff77f1e5b9202fe19c80a67e00ae2860d09df4dc
Chelsea Spangler <chelseanspangler@gmail.com>
2020-08-11 17:21:20 -0700

    set up files for hamburger menu components

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,17 +2,20 @@ import React from 'react';
 import NavBar from '../NavBar/NavBar.js';
 import styles from './Header.css';
 
-const Header = () => (
-  <>
-    <div className={styles.Header}>
-      <div className={styles.banner}>
-        <img src="header.png" />
-      </div>
-      <div id={styles.navContainer}>
+const Header = () => {
+  
+  return (
+    <>
+      <div className={styles.Header}>
+        <div className={styles.banner}>
+          <img src="header.png" />
+        </div>
+        {/* <div id={styles.navContainer}>
         <NavBar />
+      </div> */}
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default Header;
