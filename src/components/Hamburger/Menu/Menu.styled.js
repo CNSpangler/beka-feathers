04c825c57467fc1ad2d4e865247923bcb7ecac72
Chelsea Spangler <chelseanspangler@gmail.com>
2020-08-12 11:23:06 -0700

    style hamburger menu

--- src/components/Hamburger/Menu/Menu.styled.js
+++ src/components/Hamburger/Menu/Menu.styled.js
@@ -4,7 +4,9 @@ export const StyledMenu = styled.nav`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  background: ${({ theme }) => theme.primaryLight};
+  background-image: url('../../../public/marble.jpg');
+  font-family: 'Fjalla One', sans-serif;
+  font-weight: bold;
   height: 100vh;
   text-align: left;
   padding: 2rem;
@@ -24,7 +26,7 @@ export const StyledMenu = styled.nav`
     padding: 2rem 0;
     font-weight: bold;
     letter-spacing: 0.5rem;
-    color: ${({ theme }) => theme.primaryDark};
+    color: black;
     text-decoration: none;
     transition: color 0.3s linear;
     
@@ -34,7 +36,7 @@ export const StyledMenu = styled.nav`
     }
 
     &:hover {
-      color: ${({ theme }) => theme.primaryHover};
+      color: rgb(204,157,75);
     }
   }
 `;
