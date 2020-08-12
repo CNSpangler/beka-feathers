80e679a1b212c1ad0ad8b344284870d5ee758181
Chelsea Spangler <chelseanspangler@gmail.com>
2020-08-12 10:33:53 -0700

    hamburger menu closes if user clicks outside the menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 // import NavBar from '../NavBar/NavBar.js';
 import styles from './Header.css';
 import Burger from '../Hamburger/Burger/Burger.js';
 import Menu from '../Hamburger/Menu/Menu';
+import { useOnClickOutside } from '../../hooks';
 
 const Header = () => {
   const [open, setOpen] = useState(false);
+
+  const node = useRef(); 
+  useOnClickOutside(node, () => setOpen(false));
+
   
   return (
     <>
@@ -13,7 +18,7 @@ const Header = () => {
         <div className={styles.banner}>
           <img src="header.png" />
         </div>
-        <div>
+        <div ref={node}>
           <Burger open={open} setOpen={setOpen} />
           <Menu open={open} setOpen={setOpen} />
         </div>
